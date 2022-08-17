7e6d700ab9d30112aeece25df33c4ba807004e31
Natskashee <88684565+Natskashee@users.noreply.github.com>
2022-08-17 07:15:16 +0200

    Add files via upload

---
+++ Dadju store/dadjustore.js
@@ -0,0 +1,45 @@
+
+window.addEventListener("resize", function(){
+    var banner = document.querySelector('.banner');
+    var banner2 = document.querySelector('.banner-1');
+
+    var x = window.matchMedia("(max-width: 500px)")
+    myFunction(x)
+
+
+    function myFunction(x){
+        if(x.matches){
+            banner.style= "display: none"
+            banner2.style = "display: block"
+            banner2.classList.add('banner')
+        }   
+         else{
+            banner.style= "display:block"
+            banner2.style = "display: none"
+            banner2.classList.remove('banner')
+         }
+         
+    }
+    
+
+});
+
+let screen = window.matchMedia("(max-width:500px");
+var banner = document.querySelector('.banner');
+var banner2 = document.querySelector('.banner-1');
+
+screen.addEventListener("change", function(e){
+    if(screen.matches){
+                banner.style= "display: none"
+                banner2.style = "display: block"
+                banner2.classList.add('banner')
+            } 
+     else{
+                banner.style= "display:block"
+                banner2.style = "display: none"
+                banner2.classList.remove('banner')
+             }
+             
+});
+
+
